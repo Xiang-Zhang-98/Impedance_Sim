65c37bc093df352adbc4aa957ca0626e011be19f
zx <xiang_zhang_98@berkeley.edu>
2023-04-05 10:43:36 -0700

    door init

--- impedance_envs/impedance_envs/__init__.py
+++ impedance_envs/impedance_envs/__init__.py
@@ -5,6 +5,7 @@ register(id='Fanuc_peg_in_hole-v0', entry_point='impedance_envs.envs.Fanuc_peg_h
 register(id='Fanuc_pivoting-v0', entry_point='impedance_envs.envs.Fanuc_pivoting:Fanuc_pivoting')
 register(id='Fanuc_pivoting-v2', entry_point='impedance_envs.envs.Fanuc_pivoting_v2:Fanuc_pivoting')
 register(id='Fanuc_pivoting_easy-v0', entry_point='impedance_envs.envs.Fanuc_pivoting_easy_setup:Fanuc_pivoting')
+register(id='Fanuc_door-v0', entry_point='impedance_envs.envs.Fanuc_door:Fanuc_pivoting')
 
 from .envs.source import  trajectory_cubic
 from .envs.source import  lrmate_kine_base
